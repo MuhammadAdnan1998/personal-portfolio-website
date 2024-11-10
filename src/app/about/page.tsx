aecd8c2db397157c9ba6c604381ad8fbfaf8fabb
MuhammadAdnan1998 <adnanshaikh84482@gmail.com>
2024-11-10 21:21:58 +0500

    Updated page file

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,10 +7,10 @@ export default function page() {
         <h1 className="about-title">About Me</h1>
       </div>
       <div className="about-data">
-        I am Muhammad Adnan&lsquo; a Mechatronics Engineer who graduated from Mehran UET&lsquo; Jamshoro&lsquo; in 2021. I&apos;m skilled in web development&lsquo; with expertise spanning HTML5&lsquo; CSS3&lsquo; JavaScript&lsquo; TypeScript&lsquo; Firebase&lsquo; React.js&lsquo; and Next.js. I&apos;m working on expanding my knowledge in cloud-applied generative AI engineering.<br />
-        Professionally&lsquo; I&apos;ve worked in field services and instrumentation engineering roles&lsquo; and I&apos;ve recently advanced to the position of Senior Field Services Engineer. I also have entrepreneurial aspirations and am working on building an IT services company in the USA&lsquo; focusing on web development&lsquo; app development&lsquo; social media marketing&lsquo; and AI frameworks.<br />
-        I&apos;m active on GitHub&lsquo; where I showcase projects like a car rental website&lsquo; a collection of HTML and CSS projects&lsquo; and a resume builder. I&apos;m currently participating in a hackathon challenge to create an Interactive Resume Builder in TypeScript.<br />
-        Additionally&lsquo; I&apos;m working on a curriculum that covers a wide range of web and mobile application development topics. I&apos;m driven&lsquo; supportive of family&lsquo; and dedicated to achieving expertise in both technical and leadership skills.
+        I am Muhammad Adnan&lsquo; a Mechatronics Engineer who graduated from Mehran UET&lsquo; Jamshoro&lsquo; in 2021&rsquo; I&apos;m skilled in web development&lsquo; with expertise spanning HTML5&lsquo; CSS3&lsquo; JavaScript&lsquo; TypeScript&lsquo; Firebase&lsquo; React&rsquo;js&lsquo; and Next&rsquo;js&rsquo; I&apos;m working on expanding my knowledge in cloud-applied generative AI engineering.<br />
+        Professionally&lsquo; I&apos;ve worked in field services and instrumentation engineering roles&lsquo; and I&apos;ve recently advanced to the position of Senior Field Services Engineer&rsquo; I also have entrepreneurial aspirations and am working on building an IT services company in the USA&lsquo; focusing on web development&lsquo; app development&lsquo; social media marketing&lsquo; and AI frameworks&rsquo;<br />
+        I&apos;m active on GitHub&lsquo; where I showcase projects like a car rental website&lsquo; a collection of HTML and CSS projects&lsquo; and a resume builder&rsquo; I&apos;m currently participating in a hackathon challenge to create an Interactive Resume Builder in TypeScript&rsquo;<br />
+        Additionally&lsquo; I&apos;m working on a curriculum that covers a wide range of web and mobile application development topics&rsquo; I&apos;m driven&lsquo; supportive of family&lsquo; and dedicated to achieving expertise in both technical and leadership skills&rsquo;
       </div>
     </div>
   );
